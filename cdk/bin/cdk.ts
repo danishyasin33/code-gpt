2153d1cb0d11222a5296a11e8f9d3f83b7678ac4
Danish <yasind@fcc.ca>
2023-10-16 16:10:12 -0600

    feat: create github actions

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -8,3 +8,5 @@ const app = new cdk.App();
 new CodeGptStack(app, 'CodeGptStack', {
   // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 });
+
+app.synth();
