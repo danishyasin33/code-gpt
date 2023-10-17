ea012549168870cd338e7880a583af366c2b8f5e
Danish <yasind@fcc.ca>
2023-10-16 18:03:30 -0600

    fix: define default account

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -6,7 +6,7 @@ import { CodeGptStack } from '../lib/code-gpt-stack';
 const app = new cdk.App();
 
 new CodeGptStack(app, 'CodeGptStack', {
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: 'us-east-1' },
 });
 
 app.synth();
